9945ed91b095967a13a8aa74eba268fad6fb6b4a
Juan Maurente <juanmaurente@gmail.com>
2023-09-11 20:05:32 +1000

    testing integration continuous deployment with github - test 2

--- src/components/CardContent/CardContent.tsx
+++ src/components/CardContent/CardContent.tsx
@@ -18,10 +18,10 @@ const CardContent = () => {
 					data-aos-offset='0'>
 					<div className='content__image'>
 						<img src={welcome} />
-						<h1>Hola</h1>
 					</div>
 					<div className='content__card content__card--black'>
 						<h1>Julia Carriquiry photography | Sydney</h1>
+						<h1>Hola</h1>
 						<p>
 							Capturing life's moments through stunning
 							photography.
