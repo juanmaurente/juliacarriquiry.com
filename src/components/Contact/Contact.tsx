d38f2c89784f45ad1b264e4843b45f431658f81f
Juan Maurente <juanmaurente@gmail.com>
2023-09-28 16:16:17 +1000

    deletion of h1 with hola and unused line in contact component

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import './Contact.scss';
 import sanitizeHtml from 'sanitize-html';
-import Button from '../Button/Button';
 
 const Contact = () => {
 	const apiKey = import.meta.env.VITE_REACT_APP_API_KEY;
