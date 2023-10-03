343b4ebfe5bcbf8d6b0148000a51b8c821d59c0b
Juan Maurente <juanmaurente@gmail.com>
2023-10-03 23:36:59 +1100

    Changed to productImages for testing purposes

--- src/components/ImageGrid/ImageGrid.tsx
+++ src/components/ImageGrid/ImageGrid.tsx
@@ -4,6 +4,14 @@ import people1 from '../../assets/images/people1.webp';
 import people2 from '../../assets/images/people2.jpg';
 import people3 from '../../assets/images/people3.jpg';
 import people4 from '../../assets/images/people4.jpg';
+import product1 from '../../assets/images/product1.jpg';
+import product2 from '../../assets/images/product2.jpg';
+import product3 from '../../assets/images/product3.jpg';
+import product4 from '../../assets/images/product4.jpg';
+import product5 from '../../assets/images/product5.jpg';
+import product6 from '../../assets/images/product6.webp';
+import product7 from '../../assets/images/product7.jpg';
+import product8 from '../../assets/images/product8.jpg';
 
 import data from '../../util/data.json';
 import { ImageKey } from '../../util/imageKey';
@@ -15,22 +23,34 @@ const ImageGrid = () => {
 		people2: people2,
 		people3: people3,
 		people4: people4,
+		product1: product1,
+		product2: product2,
+		product3: product3,
+		product4: product4,
+		product5: product5,
+		product6: product6,
+		product7: product7,
+		product8: product8,
 	};
 
 	const peopleImages = data.images.filter(
 		(image) => image.category === 'people',
 	);
 
+	const productImages = data.images.filter(
+		(image) => image.category === 'product',
+	);
+
 	const initialDisplayCount = 3; // Cantidad inicial de imágenes a mostrar
 	const [displayIndex, setDisplayIndex] = useState(0);
 
-	const visibleImages = peopleImages.slice(
+	const visibleImages = productImages.slice(
 		displayIndex,
 		displayIndex + initialDisplayCount,
 	);
 
 	const handleChevronDownClick = () => {
-		if (displayIndex + 1 < peopleImages.length - initialDisplayCount + 1) {
+		if (displayIndex + 1 < productImages.length - initialDisplayCount + 1) {
 			setDisplayIndex(displayIndex + 1);
 		} else {
 			setDisplayIndex(0); // Vuelve al principio si no hay más elementos
@@ -42,7 +62,7 @@ const ImageGrid = () => {
 			setDisplayIndex(displayIndex - 1);
 		} else {
 			// Si estás en el primer índice, muestra las últimas imágenes
-			setDisplayIndex(peopleImages.length - initialDisplayCount);
+			setDisplayIndex(productImages.length - initialDisplayCount);
 		}
 	};
 
